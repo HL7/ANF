5e394f8707a4b0421b25d3fe3076791dc4cff763
Keith E. Campbell <campbell@informatics.com>
2019-04-29 15:02:40 -0700

    Fixing missing group ids from some modules.

--- ballot-root/mvn-build.sh
+++ ballot-root/mvn-build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 source ~/.bash_profile
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_201.jdk/Contents/Home/
+export M2_HOME=/Library/Java/apache-maven
 mvn -e clean install
 osascript openpdf.scpt
